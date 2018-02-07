b0a9691a97c0eddcab082743467fb66f06c7fe84
AnnaMatsieva <ann.matsieva@gmail.com>
2018-02-07 23:18:25 +0300

    some changes

--- reactApp/main.js
+++ reactApp/main.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Header from './src/Header.jsx';
-import Content from './src/Content.jsx';
-import Footer from './src/Footer.jsx';
-/*import Modal from './src/Modal.jsx';*/
+import Header from './src/header.jsx';
+import Content from './src/content.jsx';
+import Footer from './src/footer.jsx';
 
 ReactDOM.render(<Header />, document.getElementById('header'));
 ReactDOM.render(<Content />, document.getElementById('content'));
 ReactDOM.render(<Footer />, document.getElementById('footer'));
-/*ReactDOM.render(<Modal />, document.getElementById('modal'));*/
--- reactApp/src/content.jsx
+++ reactApp/src/content.jsx
@@ -1,24 +1,65 @@
 import React from 'react';
-import goods from '../db/goods.json';
+import popularSmartphones from '../db/popular-smartphones.json';
+import smartphones from '../db/smartphones.json';
 
 class Content extends React.Component {
 	render() {
 		return (
-			<ul className="goods flex-center">
-				{
-					goods.map(function(good){
-						return <li className="good" key={good.id}>
+			<div>
+
+				<div className="goods-block">
+					<h2>Popular Smartphones</h2>
+					<ul className="goods flex-center">
+						{
+							popularSmartphones.map(function(good){
+								return <li className="good" key={good.id}>
+									<a href="#" className="good-name">{good.name}</a>
 									<a href="#"><img className="good-image" src={good.src} alt={good.alt} /></a>
 									<div>
-										<a href="#" className="good.name">{good.name}</a>
 										<p className="good-description">{good.description}</p>
-										<p className="good-price">{good.price}</p>
-										<button id="good-order">Order</button>
+										<p className="good-price">Price: <span>{good.price}$</span></p>
+										
+										<button id="myBtn good-order">Order</button>
+								        <div id="myModal" className="modal">
+								          <div className="modal-content">
+								            <span className="close">&times;</span>
+								            <p>Some text</p>
+								          </div>
+								        </div>  
 									</div>
-						</li>;						
-					})
-				}
-			</ul>
+								</li>;						
+							})
+						}
+					</ul>
+				</div>
+
+				<div className="goods-block">
+					<h2>Smartphones</h2>
+					<ul className="goods flex-center">
+						{
+							smartphones.map(function(good){
+								return <li className="good" key={good.id}>
+									<a href="#" className="good-name">{good.name}</a>
+									<a href="#"><img className="good-image" src={good.src} alt={good.alt} /></a>
+									<div>
+										<p className="good-description">{good.description}</p>
+										<p className="good-price">Price: <span>{good.price}$</span></p>
+										
+										<button id="myBtn good-order">Order</button>
+								        <div id="myModal" className="modal">
+								          <div className="modal-content">
+								            <span className="close">&times;</span>
+								            <p>Some text</p>
+								          </div>
+								        </div>  
+									</div>
+								</li>;						
+							})
+						}
+					</ul>
+				</div>
+
+			</div>
 		);
 	}
 }
